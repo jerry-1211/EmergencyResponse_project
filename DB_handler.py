f4ad27419d4851ad4b4b677388dd5a0fff787ff9
jerry <kcjerim1234567@gmail.com>
2024-04-15 15:50:24 +0900

    feat_회원가입기능 구현 - firebase 데이터 연결 - /signin 페이지에서 회원가입 기능 구현 - 아이디 중복되는 경우만 회원가입 제한 (이외 회가입 조건  제한없음) - css/html 약간 수정

--- DB_handler.py
+++ DB_handler.py
@@ -8,3 +8,22 @@ class DBModule :
             config = json.load(f)     
         firebase = pyrebase.initialize_app(config)
         self.db = firebase.database()
+
+    def signin(self,name,_id_,pwd,phoneNumber):
+        informations = {
+            "uname" : name,
+            "pwd" : pwd,
+            "phoneNumber" : phoneNumber
+        } 
+        if self.signin_verification(_id_):
+            self.db.child("users").child(_id_).set(informations)
+            return True
+        else :
+            return False
+        
+    def signin_verification(self,uid):
+        users = self.db.child("users").get().val()
+        for i in users:
+            if uid == i :
+                return False
+        return True
--- app.py
+++ app.py
@@ -13,10 +13,21 @@ def index():
 
 @app.route("/signin")
 def signin():
-   
     return render_template("signin.html")
 
 
+@app.route("/signin_done",methods=["GET"])
+def signin_done():
+    name = request.args.get("signin_name")
+    uid = request.args.get("signin_id")
+    pwd = request.args.get("signin_pwd")
+    phoneNumber = request.args.get("signin_phoneNumber")
+    if DB.signin(name=name,_id_=uid,pwd=pwd,phoneNumber=phoneNumber):
+         return redirect(url_for("index"))
+    else :
+        flash("이미 존재하는 아이디입니다.")
+        return redirect(url_for("signin"))
+    
 
 if __name__ == "__main__":
     app.run(port=5500,host="0.0.0.0",debug=True)
